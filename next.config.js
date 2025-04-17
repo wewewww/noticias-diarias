4aada639c0f97ebfb7503baed87655973baab082
wewewww <106012785+wewewww@users.noreply.github.com>
2025-04-17 03:21:33 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const micromatch = require('micromatch');
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -7,10 +10,19 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true
-}
-
+    ignoreBuildErrors: true,
+  },
+  experimental: {
+    manuallyTraceFiles: true,
+    traceIgnorePatterns: [
+      '**/*.log',
+      '**/*.tmp',
+      '**/node_modules/**',
+      '**/.next/**',
+      '**/dist/**',
+      '**/out/**'
+    ]
+  }
 };
 
 module.exports = nextConfig;
-
